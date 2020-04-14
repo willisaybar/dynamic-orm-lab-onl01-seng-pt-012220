cf9c9f9cda4d216e9a9b390ba4f6a9b5654281b1
Willis Aybar <willisaybar@gmail.com>
2020-04-14 00:06:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,7 +20,7 @@ class InteractiveRecord
     column_names.compact
   end
 
-  self.column_names.each do |col_name|
+  def self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
@@ -29,5 +29,7 @@ class InteractiveRecord
       self.send("#{property}=", value)
     end
   end
+
+
 
 end
