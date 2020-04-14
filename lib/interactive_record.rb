6aa93bac7e8880e288114acad4916b544572a4da
Willis Aybar <willisaybar@gmail.com>
2020-04-14 00:08:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,7 +26,7 @@ class InteractiveRecord
 
   def initialize(options={})
     options.each do |property, value|
-      self.send("#{property}=", value)
+      self.send("#{property}", value)
     end
   end
 
