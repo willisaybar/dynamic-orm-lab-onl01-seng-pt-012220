6a9008cf449eda1e68871310a2d14cf03d3975dc
Willis Aybar <willisaybar@gmail.com>
2020-04-14 00:18:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,27 +4,25 @@ require 'active_support/inflector'
 class InteractiveRecord
 
   def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  def self.column_names
-    DB[:conn].results_as_hash = true
-
-    sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+      self.to_s.downcase.pluralize
     end
-    column_names.compact
-  end
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+    def self.column_names
+      DB[:conn].results_as_hash = true
 
-  def initialize(options={})
+      sql = "PRAGMA table_info('#{table_name}')"
+
+      table_info = DB[:conn].execute(sql)
+      column_names = []
+
+      table_info.each do |column|
+        column_names << column["name"]
+      end
+
+      column_names.compact
+    end
+
+    def initialize(options={})
       options.each do |property, value|
         self.send("#{property}=", value)
       end
